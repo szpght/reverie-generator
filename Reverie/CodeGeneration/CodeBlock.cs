c4785e536cac4212bb3481ef0671546303a38287
Paweł Pietrasz <szpght@gmail.com>
2017-07-19 00:54:54 +0200

    Label generation and CodeBlock

--- Reverie/CodeGeneration/CodeBlock.cs
+++ Reverie/CodeGeneration/CodeBlock.cs
@@ -1,15 +1,34 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Build.Tasks;
 
 namespace Reverie.CodeGeneration
 {
-    public class CodeBlock
+    public class CodeBlock : ICode
     {
         public IList<ICode> Code { get; set; }
         public Variable Result { get; set; }
+        public Label BeginLabel { get; set; }
+        public Label EndLabel { get; set; }
 
-        public CodeBlock()
+        public CodeBlock(IList<ICode> code, Variable result)
         {
-            
+            Code = code;
+            Result = result;
+            BeginLabel = Label.New(true);
+            EndLabel = Label.New(true);
+        }
+
+        public Assembly Generate(Context ctx)
+        {
+            var asm = new Assembly();
+            asm.Add(BeginLabel.Declaration);
+            foreach (var code in Code)
+            {
+                asm.Add(code.Generate(ctx));
+            }
+            asm.Add(EndLabel.Declaration);
+            return asm;
         }
     }
 }
--- Reverie/CodeGeneration/Label.cs
+++ Reverie/CodeGeneration/Label.cs
@@ -3,15 +3,33 @@
     public class Label
     {
         public string Name { get; }
+        public bool Local { get; }
+        public string Declaration => Name + ":";
 
-        public Label(string name)
+        private static ulong Count = 1;
+
+        public Label(string name) : this(name, false) {}
+
+        public Label(string name, bool local)
         {
+            Local = local;
             Name = name;
+            if (local)
+            {
+                Name = "." + Name;
+            }
         }
 
         public override string ToString()
         {
             return Name;
+        }
+
+        public static Label New(bool local)
+        {
+            var label = new Label("L" + Count);
+            Count += 1;
+            return label;
         }
     }
 }

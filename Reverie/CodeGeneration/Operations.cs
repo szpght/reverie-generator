ef616ed03d467e59bec610d695109571be8c20bb
Paweł Pietrasz <szpght@gmail.com>
2017-07-20 17:30:36 +0200

    jump generation works for relations in relations*
    * but if last check jumps to else, it won't work

--- Reverie/CodeGeneration/Operations.cs
+++ Reverie/CodeGeneration/Operations.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.Eventing.Reader;
+using System.Linq;
 
 namespace Reverie.CodeGeneration
 {
@@ -166,6 +168,9 @@ namespace Reverie.CodeGeneration
         public RelationType Type { get; private set; }
         public string Jump => null;
 
+        private Relation LeftAsRelation => Left as Relation;
+        private Relation RightAsRelation => Right as Relation;
+
         public Relation(IPredicate left, IPredicate right, RelationType type, bool negated = false)
         {
             Left = left;
@@ -174,27 +179,77 @@ namespace Reverie.CodeGeneration
             Negated = negated;
         }
 
+        /* wstępny algorytm:
+         * 1. przechodzenie drzewa post-order
+         * 2. jeśli węzeł jest andem, to zmieniam go na nor i neguję dzieci
+         *   2.1. Negując dziecko zmieniam nor na or i odwrotnie
+         * 3. po przejściu całego drzewa mam tylko ory i nory
+         * 4. przechodzę drzewo znowu i jeśli spotykam nora to neguję JumpToElse wszystkim wychodzącym z niego liściom
+         */
         public void Convert()
+        {
+            var relations = new List<Relation>();
+            MakePostOrderListOfRelations(relations);
+            foreach (var relation in relations)
+            {
+                relation.ConvertRelationType();
+            }
+            foreach (var relation in relations.Where(x => x.Type == RelationType.Nor && !x.Negated|| x.Type == RelationType.Or && x.Negated))
+            {
+                relation.NegateJumpInLeaves();
+            }
+        }
+
+        private void MakePostOrderListOfRelations(List<Relation> relations)
+        {
+            LeftAsRelation?.MakePostOrderListOfRelations(relations);
+            RightAsRelation?.MakePostOrderListOfRelations(relations);
+            relations.Add(this);
+        }
+
+        private void ConvertRelationType()
         {
             if (Type == RelationType.And)
             {
                 Type = RelationType.Nor;
-                Left.Negated = !Left.Negated;
-                Right.Negated = !Right.Negated;
-                ElsifyChildren();
+                if (LeftAsRelation != null)
+                {
+                    LeftAsRelation?.FlipType();
+                    RightAsRelation?.FlipType();
+                }
+                else
+                {
+                    Left.Negated = !Left.Negated;
+                    Right.Negated = !Right.Negated;
+                }
             }
-            var left = Left as Relation;
-            left?.Convert();
-            var right = Right as Relation;
-            right?.Convert();
         }
 
-        private void ElsifyChildren()
+        private void FlipType()
         {
-            Left.JumpToElse = !Left.JumpToElse;
-            Right.JumpToElse = !Right.JumpToElse;
-            (Left as Relation)?.ElsifyChildren();
-            (Right as Relation)?.ElsifyChildren();
+            if (Type == RelationType.Or)
+            {
+                Type = RelationType.Nor;
+            }
+            else if (Type == RelationType.Nor)
+            {
+                Type = RelationType.Or;
+            }
+            else
+            {
+                throw new Exception("this is supposed to be Or or Nor");
+            }
+        }
+
+        private void NegateJumpInLeaves()
+        {
+            LeftAsRelation?.NegateJumpInLeaves();
+            RightAsRelation?.NegateJumpInLeaves();
+            if (LeftAsRelation == null)
+            {
+                Left.JumpToElse = !Left.JumpToElse;
+                Right.JumpToElse = !Right.JumpToElse;
+            }
         }
 
         public Assembly Generate(Context ctx)

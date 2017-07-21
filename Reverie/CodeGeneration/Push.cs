9451f443bedea095a9266770fd492914ef7dec71
Paweł Pietrasz <szpght@gmail.com>
2017-07-21 23:46:46 +0200

    added Push operation

---
+++ Reverie/CodeGeneration/Push.cs
@@ -0,0 +1,20 @@
+﻿namespace Reverie.CodeGeneration
+{
+    public class Push : ICode
+    {
+        public Variable Variable { get; }
+
+        public Push(Variable variable)
+        {
+            Variable = variable;
+        }
+
+        public Assembly Generate(Context ctx)
+        {
+            var asm = new Assembly();
+            var register = ctx.Load(Variable, asm);
+            asm.Add($"push {register.FullName}");
+            return asm;
+        }
+    }
+}
